4d7a282ba51666e34d1f791812e5c1d501ca89a7
E.R. Salter <ers66@cam.ac.uk>
2023-05-31 20:42:05 +0100

    I think this has fixed the state switching error now it reads an analogue pin on the nterrupt

--- door_system_with_interrupts/door_system_with_interrupts.ino
+++ door_system_with_interrupts/door_system_with_interrupts.ino
@@ -1,47 +1,54 @@
 # include <Wire.h>
 const int buzzerPin = 8;
-const int interruptPin = 2;
-volatile bool state = HIGH;
+const int interruptPin = 3;
+volatile bool state_door = LOW;
+const int check_pin = A0;
 
 unsigned long time_pressed;
 
-int counter;
+int state_of_switch;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(interruptPin, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), Switch1, CHANGE); //falling means goes from open to closed (HIGH to LOW)
+  attachInterrupt(digitalPinToInterrupt(interruptPin), Switch1, CHANGE); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Serial.println("working");
-  
-  //state = digitalRead(interruptPin);
-  if(state == LOW){
-    Serial.println("about to turn light on");
-    if (millis()- time_pressed > 3000){
-      Serial.println("turning light on");
-      digitalWrite(buzzerPin, HIGH);
-    }
-    else{
-      digitalWrite(buzzerPin, LOW);
-    }
-  }
-  else{
-    digitalWrite(buzzerPin, LOW);
+
+  if(state_door == LOW){
+    
+    timer_buzzer();
   }
   
 }
 
 void Switch1(){
-  Serial.println("button pressed");
-  state = !state;
+  
+  //state_door = !state_door;
+  //state_door = digitalRead(interruptPin);
   time_pressed = millis();
-  Serial.println(state);
+  Serial.println("state door : " + String(state_door));
+  digitalWrite(buzzerPin, LOW);
+  state_of_switch = analogRead(check_pin);
+  Serial.println(state_of_switch);
+  if(state_of_switch > 30){
+    state_door = 1;
+  }
+  else{
+    state_door = 0;
+  }
   
   
+}
+
+void timer_buzzer(){
+  if (millis()- time_pressed > 3000){
+      digitalWrite(buzzerPin, HIGH);
+    }
+
 }
